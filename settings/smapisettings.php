6e53924a5595cb3d0e162466c69c6e528163f0f5
HakanBergman <hakan.bergman@drumedar.se>
2019-10-30 23:02:27 +0100

    Updated

--- settings/smapisettings.php
+++ settings/smapisettings.php
@@ -15,8 +15,8 @@
         }
     }
     /* Check if we have any credentials stored */
-    $smapi_username = get_option("smapi_username");
-    $smapi_password = get_option("smapi_password");
+    $smapi_username = get_option("stored_smapi_username");
+    $smapi_password = get_option("stored_smapi_password");
     echo $smapi_username . " " . $smapi_password;
     /* Fetch our stored values from database */
     global $wpdb;
