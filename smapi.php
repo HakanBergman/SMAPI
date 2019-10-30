a472b4f8d0085b613f2fd57bf053754549ac5235
HakanBergman <hakan.bergman@drumedar.se>
2019-10-30 21:12:21 +0100

    Updated

--- smapi.php
+++ smapi.php
@@ -65,6 +65,49 @@ register_deactivation_hook( __FILE__, 'deactivate_smapi' );
 require plugin_dir_path( __FILE__ ) . 'includes/class-smapi.php';
 
 /**
+ * Add SMAPI tables to the WP Database (if not exists)
+ * Call the function when SMAPI is activated
+ */
+$smapi_db_version = '0.5';
+$smapi_db_installed = get_option( "jal_db_version");
+function smapi_table() {
+	if ( $smapi_db_installed != $smapi_db_version) {
+		global $wpdb;
+		global $smapi_db_version;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . "smapi";
+
+		/* Create the table */
+		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
+		month_12 varchar(4) NOT NULL,
+		month_24 varchar(4) NOT NULL,
+		month_36 varchar(4) NOT NULL,
+		month_48 varchar(4) NOT NULL,
+		month_60 varchar(4) NOT NULL
+		) $charset_collate;";	
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		/* Insert sample data */
+		$wpdb->insert( 
+			$table_name, 
+			array( 
+				'month_12' => '1.0', 
+				'month_24' => '1.0',
+				'month_36' => '1.0',
+				'month_48' => '1.0',
+				'month_60' => '1.0',
+			) 
+		);	
+
+		/* Tell WordPress this script has been run */
+		$smapi_db_version = '1.0';
+		add_option( 'smapi_db_version', $smapi_db_version);
+	}
+}
+register_activation_hook( __FILE__, 'smapi_table' );
+
+/**
  * Add SMAPI settings to the admin menu
  */
 
