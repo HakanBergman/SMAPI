4c661718bc909c431e3d5adcbaf9dea452b0ed81
HakanBergman <hakan.bergman@drumedar.se>
2019-10-30 22:31:43 +0100

    Updated

--- json/fetch_data.php
+++ json/fetch_data.php
@@ -1,6 +1,27 @@
 <?php
+    
+    /* SMAPI Settings */
+    $postData = array('email' => $_GET['username'], 'password' => $_GET['password']);
     $json_url = "http://snurra.datadelenhc.com/api/v1/login";
-    $json_result = file_get_contents($json_url);
-    var_dump(json_decode($json_result));
-    echo "something was posted";    
+
+    /* Setup cURL */
+    $ch = curl_init($json_url);
+    curl_setopt_array($ch, array(
+        CURLOPT_POST => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: '.$authToken,
+            'Content-Type: application/json'
+        ),
+        CURLOPT_POSTFIELDS => json_encode($postData)
+    ));
+
+    /* Send cURL Request */
+    $response = curl_exec($ch);
+    if($response === FALSE) { die(curl_error($ch)); }
+    
+    /* Decode the result */
+    $responseData = json_decode($response, TRUE);
+    print_r($responseData);
+
 ?>
