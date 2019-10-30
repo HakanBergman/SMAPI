5ce09bbacf37ecaf57be726145b881e727d0fa4a
HakanBergman <hakan.bergman@drumedar.se>
2019-10-30 21:17:59 +0100

    Updated

--- smapi.php
+++ smapi.php
@@ -71,6 +71,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-smapi.php';
 $smapi_db_version = '0.5';
 $smapi_db_installed = get_option( "smapi_db_version");
 function smapi_table() {
+	echo $smapi_db_installed . " < > " . $smapi_db_version;
 	die("hit 23");
 	if ( $smapi_db_installed != $smapi_db_version) {
 		die("hit");
