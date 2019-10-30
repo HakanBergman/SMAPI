a3a5b95b5985b0853f39f6f53ed8873fd936d842
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:49:18 +0100

    Updated

--- json/insert_data.php
+++ json/insert_data.php
@@ -7,6 +7,10 @@
         /* Make sure our unique key is identified for this installation */
         if ($smapi_unique_key == $_POST['key']) {
             print_r($_POST);
+            /* Update our db table with the fetched values */
+            global $wpdb;
+            $table_name  = $wpdb->prefix."smapi";
+            $wpdb->query( $wpdb->prepare("UPDATE $table_name SET month_12 = $_POST['month_12'], month_24 = $_POST['month_24'], month_36 = $_POST['month_36'], month_48 = $_POST['month_48'], month_60 = $_POST['month_60']"));
         } else {
             die('Unauthorized access');
         }
