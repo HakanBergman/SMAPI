cf5626f802ce6adc6f7e44257ce739fd9cbd8e66
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:58:13 +0100

    Updated

--- json/insert_data.php
+++ json/insert_data.php
@@ -10,9 +10,8 @@
             /* Update our db table with the fetched values */
             global $wpdb;
             $table_name = $wpdb->prefix."smapi";
-            $month_12 = array('month_12' => $_POST['month_12']);
+            $month_12 = array('month_12' => '$_POST["month_12"]');
             $wpdb->update($table_name, $month_12); 
-            exit( var_dump( $wpdb->last_query ) );
         } else {
             die('Unauthorized access');
         }
