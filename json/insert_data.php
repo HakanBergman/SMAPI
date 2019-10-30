3b756d7a297a6d9e3b70a3199b4b71bd91154d4b
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:44:52 +0100

    Updated

--- json/insert_data.php
+++ json/insert_data.php
@@ -3,6 +3,8 @@
     include '/wp-load.php';
     /* Make sure this is called by an administrator */
     if (current_user_can('administrator')) {
+        $smapi_unique_key = get_option("smapi_unique_key");
+        echo $smapi_unique_key;
         print_r($_POST);
     } else {
         die('Unauthorized access');
