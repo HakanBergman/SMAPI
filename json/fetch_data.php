2f5d7909b982dc38186774496c58a99114aeab81
HakanBergman <hakan.bergman@drumedar.se>
2019-10-30 22:27:24 +0100

    Updated

--- json/fetch_data.php
+++ json/fetch_data.php
@@ -1,6 +1,6 @@
 <?php
     $json_url = "http://snurra.datadelenhc.com/api/v1/login"
-    $json_result = file_get_contents($json_url);
-    var_dump(json_decode($json_result));
+    #$json_result = file_get_contents($json_url);
+    #var_dump(json_decode($json_result));
     echo "something was posted";    
 ?>
