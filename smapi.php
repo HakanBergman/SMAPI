f95eb2608e2d57fff3e43e75914b09eac6e3e4c3
HakanBergman <hakan.bergman@drumedar.se>
2019-10-30 19:40:31 +0100

    Adding files
    Added files

---
+++ smapi.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://www.datadelenhc.se
+ * @since             1.0.0
+ * @package           Smapi
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Smarprovider API
+ * Plugin URI:        https://github.com/HakanBergman/SMAPI.git
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Datadelen Hosting Center
+ * Author URI:        https://www.datadelenhc.se
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       smapi
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'SMAPI_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-smapi-activator.php
+ */
+function activate_smapi() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-smapi-activator.php';
+	Smapi_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-smapi-deactivator.php
+ */
+function deactivate_smapi() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-smapi-deactivator.php';
+	Smapi_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_smapi' );
+register_deactivation_hook( __FILE__, 'deactivate_smapi' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-smapi.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_smapi() {
+
+	$plugin = new Smapi();
+	$plugin->run();
+
+}
+run_smapi();
