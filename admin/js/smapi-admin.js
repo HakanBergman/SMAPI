5df9422662eb724400ecee722180197af28d159a
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:39:53 +0100

    Updated

--- admin/js/smapi-admin.js
+++ admin/js/smapi-admin.js
@@ -56,16 +56,16 @@
 					$('#month_36').val(month_36);
 					$('#month_48').val(month_48);
 					$('#month_60').val(month_60);
+					/* Update database */
+					$.post( "/wp-content/plugins/SMAPI/json/insert_data.php", { month_12: month_12, month_24: month_24, month_36: month_36, month_48: month_48, month_60: month_60 })
+					.done(function( data ) {
+						//	console.log(data);
+						/* Enable button for clicks */
+						$('.updatefinancialvalues').attr("disabled", false);
+					});					
 				}
-				/* Enable button for clicks */
-				$('.updatefinancialvalues').attr("disabled", false);				
-			});	
-			/* Update database */
-			//$.post( "/wp-content/plugins/SMAPI/json/insert_data.php", { month_12: month_12, month_24: month_24, month_36: month_36, month_48: month_48, month_60: month_60 })
-			//.done(function( data ) {
-			//	console.log(data);
-			//});				
-			
+				
+			});								
 		});
 	});
 
