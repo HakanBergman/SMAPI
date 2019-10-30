7c53ce9a3ea70c911e6e2d823d442343d57c143c
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:46:05 +0100

    Updated

--- json/insert_data.php
+++ json/insert_data.php
@@ -4,8 +4,12 @@
     /* Make sure this is called by an administrator */
     if (current_user_can('administrator')) {
         $smapi_unique_key = get_option("smapi_unique_key");
-        echo $smapi_unique_key;
-        print_r($_POST);
+        /* Make sure our unique key is identified for this installation */
+        if ($smapi_unique_key == $_POST['key']) {
+            print_r($_POST);
+        } else {
+            die('Unauthorized access');
+        }
     } else {
         die('Unauthorized access');
     }
