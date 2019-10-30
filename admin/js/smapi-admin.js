a2fec74fcf94029817faf1cb9554790fea054096
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:22:39 +0100

    Updated

--- admin/js/smapi-admin.js
+++ admin/js/smapi-admin.js
@@ -56,11 +56,30 @@
 					$('#month_36').val(month_36);
 					$('#month_48').val(month_48);
 					$('#month_60').val(month_60);
-					console.log(month_12 + ' ' + month_24 + ' ' + month_36 + ' ' + month_48 + ' ' + month_60);
+				}
+			});	
+			/* Update database */
+			$.post( "/wp-content/plugins/SMAPI/json/insert_data.php", { email: smapi_username, password: smapi_password })
+			.done(function( data ) {
+				var json_string = JSON.stringify(data);
+				var json_obj = jQuery.parseJSON(json_string);
+				if (json_obj.success) {
+					var month_12 = json_obj.success.month_12;
+					var month_24 = json_obj.success.month_24;
+					var month_36 = json_obj.success.month_36;
+					var month_48 = json_obj.success.month_48;
+					var month_60 = json_obj.success.month_60;	
+					$('#month_12').val(month_12);
+					$('#month_24').val(month_24);
+					$('#month_36').val(month_36);
+					$('#month_48').val(month_48);
+					$('#month_60').val(month_60);
 				}
 				/* Enable button for clicks */
 				$('.updatefinancialvalues').attr("disabled", false);
-			});		
+			});				
+			/* Enable button for clicks */
+			$('.updatefinancialvalues').attr("disabled", false);			
 		});
 	});
 
