98b27db8291955577a3a2ebf3db83cd81a9736e8
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:43:54 +0100

    Updated

--- json/insert_data.php
+++ json/insert_data.php
@@ -3,7 +3,7 @@
     include '/wp-load.php';
     /* Make sure this is called by an administrator */
     if (current_user_can('administrator')) {
-        echo "done";
+        print_r($_POST);
     } else {
         die('Unauthorized access');
     }
