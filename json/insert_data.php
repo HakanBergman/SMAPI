da3ec1b79e470a8c778ed8dd4f8707050fcd281f
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 00:41:56 +0100

    Updated

--- json/insert_data.php
+++ json/insert_data.php
@@ -4,6 +4,8 @@
     /* Make sure this is called by an administrator */
     if (current_user_can('administrator')) {
         echo "done";
+    } else {
+        die('Unauthorized access');
     }
 
 ?>
