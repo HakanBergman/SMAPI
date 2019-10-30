43ea10f00d3d364912f8335180f8792868643f2b
HakanBergman <hakan.bergman@drumedar.se>
2019-10-30 20:05:09 +0100

    Updated

--- smapi.php
+++ smapi.php
@@ -33,8 +33,7 @@ if ( ! defined( 'WPINC' ) ) {
 add_action( 'admin_menu', 'my_admin_menu' );
 
 function my_admin_menu() {
-	add_menu_page( 'My Top Level Menu Example', 'Top Level Menu', 'manage_options', 'myplugin/myplugin-admin-page.php', 'myplguin_admin_page', 'dashicons-tickets', 6  );
-	add_submenu_page( 'myplugin/myplugin-admin-page.php', 'My Sub Level Menu Example', 'Sub Level Menu', 'manage_options', 'myplugin/myplugin-admin-sub-page.php', 'myplguin_admin_sub_page' ); 
+	add_menu_page( 'My Top Level Menu Example', 'Top Level Menu', 'manage_options', 'SMAPI/myplugin-admin-page.php', 'myplguin_admin_page', 'dashicons-tickets', 6  );
 }
 
 /**
