569a38a45a22ad9722ec3caec4091808074261dd
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 02:06:29 +0100

    Updated

--- shortcode/smapi_calculator.php
+++ shortcode/smapi_calculator.php
@@ -9,7 +9,7 @@
         $month_36 = $wpdb->query("SELECT month_36 FROM $table_name LIMIT 1");
         $month_48 = $wpdb->query("SELECT month_48 FROM $table_name LIMIT 1");
         $month_60 = $wpdb->query("SELECT month_60 FROM $table_name LIMIT 1");
-        print_r($month_24);
+        var_dump($month_24);
         echo "test";
         $content =  '<form method="post" action="" id="smapi_calculator">';
             $content .= '<div class="form-group">';
