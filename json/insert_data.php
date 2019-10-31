fe633a567b075099de7fe09112259f74872611c1
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 01:10:03 +0100

    Updated

--- json/insert_data.php
+++ json/insert_data.php
@@ -1,6 +1,5 @@
 <?php
 
-    define( 'SHORTINIT', true );
     include '/wp-load.php';
     /* Make sure this is called by an administrator */
     if (current_user_can('administrator')) {
@@ -11,7 +10,7 @@
             /* Update our db table with the fetched values */
             global $wpdb;
             $table_name = $wpdb->prefix."smapi";
-            $wpdb->query($wpdb->prepare("UPDATE $table_name SET month_12='2.9'"));
+            $wpdb->query($wpdb->prepare("UPDATE $table_name SET month_12 = '2.9'"));
         } else {
             die('Unauthorized access');
         }
