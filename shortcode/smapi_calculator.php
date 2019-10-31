8d48f14ddd8cf097f6575a622a945140826ed4fb
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 01:49:56 +0100

    Updated

--- shortcode/smapi_calculator.php
+++ shortcode/smapi_calculator.php
@@ -9,6 +9,27 @@
             $content .= '<div class="form-group">';
                 $content .= '<button type="button" name="smapi_button">Skicka offert</button>';
             $content .= '</div>';
+            $content .= '<hr />';
+            $content .= '<div class="form-group">';
+                $content .= '<label for="smapi_amount">Månadskostnad över 12 månader</label>';
+                $content .= '<div><input type="number" name="smapi_month_12" id="smapi_month_12" placeholder="0" disabled/></div>';
+            $content .= '</div>';
+            $content .= '<div class="form-group">';
+                $content .= '<label for="smapi_amount">Månadskostnad över 24 månader</label>';
+                $content .= '<div><input type="number" name="smapi_month_24" id="smapi_month_24" placeholder="0" disabled/></div>';
+            $content .= '</div>';
+            $content .= '<div class="form-group">';
+                $content .= '<label for="smapi_amount">Månadskostnad över 36 månader</label>';
+                $content .= '<div><input type="number" name="smapi_month_36" id="smapi_month_36" placeholder="0" disabled/></div>';
+            $content .= '</div>';
+            $content .= '<div class="form-group">';
+                $content .= '<label for="smapi_amount">Månadskostnad över 48 månader</label>';
+                $content .= '<div><input type="number" name="smapi_month_48" id="smapi_month_48" placeholder="0" disabled/></div>';
+            $content .= '</div>';   
+            $content .= '<div class="form-group">';
+                $content .= '<label for="smapi_amount">Månadskostnad över 60 månader</label>';
+                $content .= '<div><input type="number" name="smapi_month_60" id="smapi_month_60" placeholder="0" disabled/></div>';
+            $content .= '</div>';                                             
         $content .= '</form>';
         return $content;
     }
