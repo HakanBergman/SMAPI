14ce440ac5819b5822d1b74ff3d5056fa5342d29
HakanBergman <hakan.bergman@drumedar.se>
2019-10-31 01:38:13 +0100

    Updated

--- shortcode/smapi_calculator.php
+++ shortcode/smapi_calculator.php
@@ -1,16 +1,16 @@
 <?php 
 
     function smapi_calculator() {
-        $content =  '<form method="post" action="" id="smapi_calculator">';
-            $content .= '<div class="form-group">';
-                $content .= '<label for="smapi_amount">Hur mycket vill du låna?</label>';
-                $content .= '<div><input type="number" name="smapi_amount" id="smapi_amount" placeholder="0" /></div>';
-            $content .= '</div>';
-            $content .= '<div class="form-group">';
-                $content .= '<button type="button" name="smapi_button">Skicka offert</button>';
-            $content .= '/div>';
-        $content .= '</form>';
-        return $content;
+    #    $content =  '<form method="post" action="" id="smapi_calculator">';
+    #        $content .= '<div class="form-group">';
+    #            $content .= '<label for="smapi_amount">Hur mycket vill du låna?</label>';
+    #            $content .= '<div><input type="number" name="smapi_amount" id="smapi_amount" placeholder="0" /></div>';
+    #        $content .= '</div>';
+    #        $content .= '<div class="form-group">';
+    #            $content .= '<button type="button" name="smapi_button">Skicka offert</button>';
+    #        $content .= '</div>';
+    #    $content .= '</form>';
+    #    return $content;
     }
 
 } ?>
