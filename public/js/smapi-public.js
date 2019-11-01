28207f68e0c8e92969b8a6e838c3057f4bd03bf0
HakanBergman <hakan.bergman@drumedar.se>
2019-11-01 15:06:00 +0100

    Updated

--- public/js/smapi-public.js
+++ public/js/smapi-public.js
@@ -35,7 +35,7 @@
 		/* Do this when user enters an amount on the financial front page */
 		$('#smapi_amount').keydown(function() {
 			var money = $(this).val();
-			var month_12 = Math.round((money*($('#smapi_interest_month_12').val()/100))/12);
+			var month_12 = Math.round((money*$('#smapi_interest_month_12').val())/12);
 			var month_24 = Math.round((money*($('#smapi_interest_month_24').val()/100))/24);
 			var month_36 = Math.round((money*($('#smapi_interest_month_36').val()/100))/36);
 			var month_48 = Math.round((money*($('#smapi_interest_month_48').val()/100))/48);
