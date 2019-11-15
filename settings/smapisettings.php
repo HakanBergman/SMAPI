602345e81be6861607237a76810112b169b3b72c
HakanBergman <hakan.bergman@drumedar.se>
2019-11-15 13:22:43 +0100

    Updated

--- settings/smapisettings.php
+++ settings/smapisettings.php
@@ -21,6 +21,7 @@
          $upload_directory = ABSPATH . 'wp-content/plugins/SMAPI/upload';
         /* Make sure upload folder is writeable */
         if (is_writable($upload_directory)) {
+            echo "hit";
             file_put_contents($upload_directory."/upload/smapi.zip", file_get_contents($download_url));
         }
     }
