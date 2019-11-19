0e4bb69011883bd8c74901120ee583738261d284
HakanBergman <hakan.bergman@drumedar.se>
2019-11-19 18:50:57 +0100

    Updated

--- shortcode/smapi_calculator.php
+++ shortcode/smapi_calculator.php
@@ -58,11 +58,11 @@ function smapi_calculator_input() {
         $smapi_results = $wpdb->get_results("SELECT * FROM $table_name LIMIT 1");
         $content =  '<form method="post" action="" id="smapi_calculator">';
             $content .= '<div class="form-group">';
-                $content .= '<input type="hidden" name="smapi_interest_month_12" id="smapi_interest_month_12" value="'.$smapi_results[0]->month_12_insurance.'" />';
-                $content .= '<input type="hidden" name="smapi_interest_month_24" id="smapi_interest_month_24" value="'.$smapi_results[0]->month_24_insurance.'" />';
-                $content .= '<input type="hidden" name="smapi_interest_month_36" id="smapi_interest_month_36" value="'.$smapi_results[0]->month_36_insurance.'" />';
-                $content .= '<input type="hidden" name="smapi_interest_month_48" id="smapi_interest_month_48" value="'.$smapi_results[0]->month_48_insurance.'" />';
-                $content .= '<input type="hidden" name="smapi_interest_month_60" id="smapi_interest_month_60" value="'.$smapi_results[0]->month_60_insurance.'" />';
+                $content .= '<input type="hidden" name="smapi_interest_month_12_insurance" id="smapi_interest_month_12_insurance" value="'.$smapi_results[0]->month_12_insurance.'" />';
+                $content .= '<input type="hidden" name="smapi_interest_month_24_insurance" id="smapi_interest_month_24_insurance" value="'.$smapi_results[0]->month_24_insurance.'" />';
+                $content .= '<input type="hidden" name="smapi_interest_month_36_insurance" id="smapi_interest_month_36_insurance" value="'.$smapi_results[0]->month_36_insurance.'" />';
+                $content .= '<input type="hidden" name="smapi_interest_month_48_insurance" id="smapi_interest_month_48_insurance" value="'.$smapi_results[0]->month_48_insurance.'" />';
+                $content .= '<input type="hidden" name="smapi_interest_month_60_insurance" id="smapi_interest_month_60_insurance" value="'.$smapi_results[0]->month_60_insurance.'" />';
             $content .= '</div>';
             $content .= '<div class="form-group">';
                 $content .= '<label for="smapi_amount">Månadskostnad över 12 månader</label>';
